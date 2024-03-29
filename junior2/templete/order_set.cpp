765810f2bc8af98ca44b517b11be8126034546a5
MagedSayeed <163421100+MagedSayeed@users.noreply.github.com>
2024-03-29 22:37:43 +0200

    Update order_set.cpp

--- junior2/templete/order_set.cpp
+++ junior2/templete/order_set.cpp
@@ -1,6 +1,7 @@
 //function (1) find_by_order -> accessing index st[1] return iterator
        // (2) order_of_key  -> (kam rakm asghyr meny)
-
+       // (3) less<int> -> lw sorted asc
+       // (4) greater<int> -> lw sorted desc
 //ordered_set
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
@@ -21,3 +22,6 @@ using namespace __gnu_pbds;
 
 using namespace __gnu_pbds;
 #define ordered_set tree<pair<int,int>, null_type,less<int>,  rb_tree_tag,tree_order_statistics_node_update>
+
+
+
