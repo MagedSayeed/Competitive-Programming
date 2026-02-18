ffe111fadcdb0dd7c64fef766819c24310111c6b
MagedSayed <163421100+MagedSayeed@users.noreply.github.com>
2026-02-18 22:41:23 +0200

    Update tmp.cpp

--- tmp.cpp
+++ tmp.cpp
@@ -1,33 +1,22 @@
-MATH(EXPR stack_size "1024 * 1024 * 1024")
-set(CMAKE_EXE_LINKER_FLAGS "-Wl,--stack,${stack_size}")
-cmake_minimum_required(VERSION 3.26)
-=================
-
 #include <bits/stdc++.h>
-/// لتكن مشيئتك
-#define int long long
-#define endl  "\n"
 using namespace std;
+#define endl '\n'
+#define int long long
+const int N=2e5+5,MOD=1e9+7;
 
-void solve() {
-  
+void solve()
+{
 
 }
 
-signed main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-
-#ifndef ONLINE_JUDGE
-    freopen("in.txt", "r", stdin);
-    freopen("out.txt", "w", stdout);
-#endif
-
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
     int tc = 1;
     cin >> tc;
-    for (int i = 1; i <= tc; ++i) {
+    while (tc--)
         solve();
-        cout << "\n";
 
-    }
+    return 0;
 }
